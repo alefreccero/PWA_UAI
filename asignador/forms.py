556aa8d80bc6e8e6ad5a335c3e9626de6f36a74a
agugar <61169272+agugar@users.noreply.github.com>
2020-03-08 18:51:09 -0300

    entrega

--- asignador/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class cursadasForm(forms.Form):
-    carrera = forms.CharField(label='Nombre de Carrera', max_length=100)
--- reporte2/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('planificador', views.reporte_planificador, name='reporte_planificador'),
-]
